aa42bfd985fcad734639a92f1e2a7a4777b0647c
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-04-01 15:09:26 +0200

    feat(Projet) : Ajout du header dasn appshell
    [50][DONE]

--- Projet/P_AppMobile/MainPage.xaml.cs
+++ Projet/P_AppMobile/MainPage.xaml.cs
@@ -2,44 +2,13 @@
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
-            Header(layout);
         }
 
         private async void OnCounterClicked(object sender, EventArgs e) {
             await Shell.Current.GoToAsync("//TestPage");
         }
 
-        public static void Header(Layout layout) {
 
-            Grid grid = new Grid {
-                RowDefinitions =
-                {
-                    new RowDefinition { Height = new GridLength(100) }
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition(),
-                    new ColumnDefinition(),
-                    new ColumnDefinition()
-                }
-            };
-
-            Label label = new Label {
-                WidthRequest = 500,
-                HeightRequest = 50,
-                Text = "test",
-                BackgroundColor = Color.FromRgb(230, 230, 230),
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Start
-            };
-
-            grid.Add(label, 0, 0);
-            grid.Add(label, 0, 2);
-
-
-            layout.Children.Insert(0, grid); // TODO : ne fonctionne pas, à regler
-
-        }
     }
 
 }
