368e4a54a27178e64acc2ea6b76887bb7f8ba84f
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-04-22 16:29:00 +0200

    feat(projet): Ajout de la librairy EpubSharp
    [180][DONE]

---
+++ Projet/P_AppMobile/Resources/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace P_AppMobile.Resources.Models {
+    public class Book {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string EpubFilePath { get; set; } = string.Empty;
+        public int FileSizeBytes { get; set; }
+        public string UploadedAt { get; set; } = string.Empty;
+    }
+}
